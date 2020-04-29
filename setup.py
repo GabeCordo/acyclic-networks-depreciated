f8662d8c6204d9f5b44d99ec2f805356f88d9beb
Gabriel Cordovado <gabe.cordovado@yahoo.ca>
2020-04-29 11:51:12 -0400

    final fix of setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ import setuptools
 with open("README.md", "r") as fh:
 	readme_description = fh.read()
 
+longDescription = 'A mid-weight framework for replicating tor entry, relay and exit nodes. Developed for programmers looking to provide highly-secure messaging/file transfer applications. Equipt with end-to-end encryption and anonymous server meshing.'
+
 setuptools.setup(
 	name = 'pynodetor',
-	packages = ['pynodetor']
+	packages = ['pynodetor'],
 	version = '0.1',
 	licence = 'MIT',
-	description = 'A mid-weight framework for replicating tor entry, relay and exit nodes. Developed for programmers looking to provide highly-secure messaging/file transfer applications. Equipt with end-to-end encryption and anonymous server meshing.',
+	description = longDescription,
 	author = 'Gabriel Cordovado',
 	author_email = 'gabriel.cordovado@icloud.com',
 	long_description = readme_description,
 	long_description_content_type = 'text/markdown',
 	url ='https://github.com/GabeCordo/python-node-tor',
-	download_url = 'https://github.com/GabeCordo/python-node-tor/archive/v_01.tar.gz'
+	download_url = 'https://github.com/GabeCordo/python-node-tor/archive/v_01.tar.gz',
 	keywords = ['TOR', 'SOCKETS', 'SECURITY', 'ENCRYPTION'],
 	install_requires = [
 		'cffi',
@@ -22,7 +24,7 @@ setuptools.setup(
 	],
 	classifiers = [
 		'License :: OSI Approved :: MIT License',
-		'Operating System :: OS Independent',
+		'Operating System :: OS Independent'
 	],
-	python_requries = '>=3.4',
+	python_requries = '>=3.4'
 )
